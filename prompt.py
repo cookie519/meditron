d4d3c2fd541a1908725f911580da5335f821c1c1
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-04-27 18:14:05 -0400

    Create prompt.py

---
+++ prompt.py
@@ -0,0 +1,10 @@
+from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
+
+model = AutoModelForCausalLM.from_pretrained("/scratch/gpfs/jx0800/meditron-7b")
+tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/jx0800/meditron-7b")
+recognizer = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+prompt = """what is diabete"""
+
+generated_text = generator(prompt, max_length=100, temperature=1, top_k=0, top_p=0)
+print(generated_text[0]["generated_text"])
