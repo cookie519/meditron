a799dd7fcc896e12deb18933a05c913c03da376b
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-04-27 18:31:21 -0400

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -3,9 +3,14 @@ from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
 model = AutoModelForCausalLM.from_pretrained("/scratch/gpfs/jx0800/meditron-7b")
 tokenizer = AutoTokenizer.from_pretrained("/scratch/gpfs/jx0800/meditron-7b")
-recognizer = pipeline("question-answering", model=model, tokenizer=tokenizer) #text-generation
+#recognizer = pipeline("question-answering", model=model, tokenizer=tokenizer) #text-generation
 
-prompt = """what is diabete"""
+#generated_text = recognizer(prompt, max_length=100, temperature=1, top_k=0, top_p=0)
+#print(generated_text[0]["generated_text"])
 
-generated_text = recognizer(prompt, max_length=100, temperature=1, top_k=0, top_p=0)
-print(generated_text[0]["generated_text"])
+prompt = "what is diabetes?"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
