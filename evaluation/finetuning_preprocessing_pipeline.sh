e52604cbbd7a0216bd8ad1a73490f9421bae1a82
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-04-27 17:24:01 -0400

    Update finetuning_preprocessing_pipeline.sh

--- evaluation/finetuning_preprocessing_pipeline.sh
+++ evaluation/finetuning_preprocessing_pipeline.sh
@@ -1,6 +1,6 @@
 # Run fine-tuning data preprocessing for all medical benchmarks
 # benchmarks: medmcqa, pubmedqa, medqa
 
-python finetuning_data_prep.py \
- -benchmark medmcqa \
- -cot
+python 	finetune_preprocessing.py \
+ --benchmark medmcqa \
+ --cot
