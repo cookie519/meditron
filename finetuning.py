ac6255d10aef44cb0fffee97f0fba51c517b6b6e
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-05-02 21:05:57 -0400

    Update finetuning.py

--- finetuning.py
+++ finetuning.py
@@ -26,9 +26,9 @@ bnb_config = BitsAndBytesConfig(
 # Load model and tokenizer
 model = AutoModelForCausalLM.from_pretrained(
     "/scratch/gpfs/jx0800/meditron-7b",
-    quantization_config=bnb_config,
-    use_cache=False,
-    use_flash_attention_2=True,
+    #quantization_config=bnb_config,
+    #use_cache=False,
+    #use_flash_attention_2=True,
     device_map="auto",
 )
 model.config.pretraining_tp = 1
