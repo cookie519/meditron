31f2e0eae39c5010ddacc2e848fca45da884ed1b
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-05-04 16:20:07 -0400

    Update finetuning.py

--- finetuning.py
+++ finetuning.py
@@ -103,7 +103,7 @@ args = TrainingArguments(
     warmup_ratio=0.03,
     lr_scheduler_type="constant",
     disable_tqdm=True, # disable tqdm since with packing values are in correct
-    report_to = "none" #"tensorboard"
+    report_to = "tensorboard" #"tensorboard"
 )
 print("args ready")
 
