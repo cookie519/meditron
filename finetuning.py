1b6a4f39fa07b5dce434f69fdf7657bbdea6ab26
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-05-03 22:59:31 -0400

    Update finetuning.py

--- finetuning.py
+++ finetuning.py
@@ -63,7 +63,7 @@ from transformers import TrainingArguments
  
 args = TrainingArguments(
     output_dir="/scratch/gpfs/jx0800/finetuned",
-    num_train_epochs=3,
+    num_train_epochs=1,
     per_device_train_batch_size=6,
     gradient_accumulation_steps=2,
     gradient_checkpointing=True,
