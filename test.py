432e2db6f59f941d5e491eb5c126f5c83377e7e5
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-05-04 19:26:58 -0400

    Update test.py

--- test.py
+++ test.py
@@ -63,7 +63,7 @@ prompt = get_prompt(sample)
 
 input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids.cuda()
 # with torch.inference_mode():
-outputs = model.generate(input_ids=input_ids, max_new_tokens=100, do_sample=True, top_p=0.9,temperature=0.9)
+outputs = model.generate(input_ids=input_ids, max_new_tokens=200, do_sample=True, top_p=0.9,temperature=0.9)
  
 print(f"Prompt:\n{prompt}\n")
 print(f"Generated instruction:\n{tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0][len(prompt):]}")
